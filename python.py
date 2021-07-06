709372f78cf3a9fc60613788234e1aeea889ebe6
gathonicatherine <catherrinewanjiku@gmail.com>
2021-07-06 15:32:51 +0000

    python_loops_boleans_if statement

---
+++ python.py
@@ -0,0 +1,54 @@
+#LOOPS
+my_list = [1,3,4,5]
+#for this case i want to add each and every item with 5
+#and am going to use a for lop
+for a in my_list: 
+    a = a +5
+    print(a)
+
+#BOOLEANS
+print(type(True))#returns a bool since its indeed a boolean
+print(type("True"))#returns a string since it has "" quotation marks
+
+# Testing whether statements are true or false
+print(5==5)
+print(3==5)
+print(1==1)
+
+# IF STATEMENT 
+a = 10
+b = 4
+if a % b ==0:
+    print(True)
+else:
+    print(False)   
+
+#WHILE LOOP(one has to have a break in the if statement)
+#it has to have two statement for it to be executed 
+number = 8
+while number <1:
+    print(number)
+    if number == 2:
+        break
+    number = number +1
+a = 100
+while (a< 50):
+    print(a)
+#   
+    
+numberr = 2
+while numberr <4:
+    print(numberr)
+    numberr = numberr +1
+else:
+    print("num numnumnum num")    
+#IF STATEMENT 
+num = 10
+if num >89:
+    print("positve number")
+elif num ==0:
+    print("Zero")
+else:
+    print("negative number")     
+
+
